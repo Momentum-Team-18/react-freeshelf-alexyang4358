5fc689b4d2c2a333a176e512ac6e9d7bd58b6f29
alexyang4358 <alexyang4358@gmail.com>
2023-06-07 15:49:10 -0400

    show more working

--- src/Book.js
+++ src/Book.js
@@ -1,15 +1,28 @@
+import { useState } from "react";
+
 function Book(props) {
-  console.log("Here is props: ", props);
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <div className="card">
       <h1>{props.book.title}</h1>
       <h3>Author: {props.book.author}</h3>
       <p>Description: {props.book.shortDescription}</p>
-      <img
-        className="image"
-        alt="coverimage"
-        src={props.book.coverImageUrl}
-      ></img>
+      <div className="cardImage">
+        <img className="image" src={props.book.coverImageUrl}></img>
+      </div>
+      <button onClick={() => setExpanded(!expanded)}>
+        {" "}
+        {expanded ? "Show Less" : "Show More"}
+      </button>
+      {expanded && (
+        <div>
+          <a href={props.book.url}>{props.book.title}</a>
+          <p>Publisher: {props.book.publisher}</p>
+          <p>Publication Date: {props.book.publicationDate}</p>
+          <p>Full description: {props.book.detailedDescription}</p>
+        </div>
+      )}
     </div>
   );
 }
